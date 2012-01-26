3020fc234cc58f3db071f3176e4487e77a6c24be
Alan Beale <alan.beale@chicagotrading.com>
2012-01-26 07:26:11 -0600

    updates to function

--- bulletExample.R
+++ bulletExample.R
@@ -20,7 +20,7 @@ require(ggplot2)
 #                axis.title.y = theme_text(angle=0,size=10))
 
 
-bulleChartHor3<-function(grades,title,target,measure,breaks) {
+bulletChartHor3Exp<-function(grades,title,target,measure,breaks) {
   grades.fill=c("grey60","grey75","grey90")
   grades.bars=c(grades[1],diff(grades))
   chart.grades<-data.frame(values=grades.bars,fill=grades.fill)
@@ -52,4 +52,36 @@ bulleChartHor3<-function(grades,title,target,measure,breaks) {
   p
 }
 
-print(bulleChartHor3(c(200,250,300),title="test",target=250,measure=270))
+bulletChartHor3<-function(grades,title,target,measure,breaks) {
+  grades.fill=c("grey60","grey75","grey90")
+  grades.bars=c(grades[1],diff(grades))
+  chart.grades<-data.frame(values=grades.bars,fill=grades.fill)
+
+ p<-ggplot(chart.grades) +
+   geom_bar(aes(x=1,y=values,fill=fill),width=0.2)+
+     geom_bar(aes_string(x=1,y=measure),fill="black",width=0.1)+
+       geom_hline(aes_string(yintercept=target))+
+         scale_fill_identity()+
+           theme_bw() +
+             scale_x_continuous(breaks=c(2))
+  
+  if(missing(breaks)) {
+    p<- p+ scale_y_continuous(expand=c(0,0))
+  }
+  else {
+    p<- p+ scale_y_continuous(breaks=breaks,expand=c(0,0)) 
+  }
+     
+  p<- p+   coord_flip() +
+    opts(legend.position="none",aspect.ratio=0.10,
+         panel.border = theme_blank(),
+         panel.background = theme_blank(),
+         panel.grid.major = theme_blank(),
+         axis.text.y =theme_blank(),
+         axis.title.x =theme_blank(),
+         axis.title.y = theme_blank())
+  
+  p
+}
+
+#print(bulleChartHor3(c(200,250,300),title="test",target=250,measure=270))
